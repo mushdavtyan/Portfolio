68190c508499586c3b89ebef6fb30402e882c225
Narek <anun.azganun.1379@gmail.com>
2021-09-21 22:50:28 +0000

    changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> MUSHEGH DAVTYAN</strong>
+                <strong className="main-name"> NAREK PAPYAN</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
